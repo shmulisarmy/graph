e56a5a3fd11f149af6860bf838c63bdbf4e060a8
Shmuli <Shmuli@Mindels-Air.attlocal.net>
2024-09-04 13:55:28 -0400

    added expandsion method

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,26 @@
 from src.graph import Grpah
 
+
 g = Grpah()
 g.addConnectionType("friend")
 g.addConnectionType("coworker")
 
+
+
+people = ["Alice", "Bob", "Carol", "David", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Judy", "Kathy", "Linda", "Mindy", "Nancy", "Oscar", "Penny", "Quincy", "Ruth", "Sarah", "Tom", "Ursula", "Victor", "Wendy", "Xavier", "Yvonne", "Zara", "Zoe"]
+
 g.addVertex("Alice")
 g.addVertex("Bob")
 g.addVertex("Carol")
+g.addVertex("David")
 
 g.addConnection("Alice", "Bob", "friend")
-g.addConnection("Alice", "Carol", "coworker")
+g.addConnection("Alice", "Carol", "friend")
 
-g.display()
 
-    
+g.addConnection("Carol", "Frank", "coworker")
+g.addConnection("Bob", "David", "coworker")
+
+
+
+print(g.expand("Alice", ["friend", "coworker", "po"]))
--- src/graph.py
+++ src/graph.py
@@ -22,3 +22,36 @@ class Grpah:
         for key, value in self.__dict__.items():
             print(key, value)
 
+    def expand(self, vertexName, connectionTypes):
+        if vertexName not in self.vertexes:
+            raise Exception("Vertex not found")
+        
+        if not connectionTypes:
+            return [vertexName]
+        
+        if not isinstance(connectionTypes, list):
+            connectionTypes = [connectionTypes]
+
+        vertexes = []
+        using_vertexes = [vertexName]
+        next_using_vertexes = []
+
+        try:
+            for connectionType in connectionTypes:
+                if connectionType not in self.connectionTypes:
+                    raise Exception(f"Connection type '{connectionType}' not found")
+                for vertex in using_vertexes:
+                    next_using_vertexes.extend(self.vertexes[vertex][connectionType])
+
+                using_vertexes = next_using_vertexes
+                next_using_vertexes = []
+        except Exception as e:
+            print(e, "however we'll return all everything up until this point")
+
+        vertexes.extend(using_vertexes)
+        return vertexes
+    
+
+
+    
+
